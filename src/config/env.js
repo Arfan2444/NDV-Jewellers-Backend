cdb9ed1752e567a100565d2abcb48de0b5aecf06
parag-47 <paragyadav74@gmail.com>
2026-03-03 01:34:14 +0530

    fix: add .env

--- src/config/env.js
+++ src/config/env.js
@@ -1,13 +1,7 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 5000,
-  mongoUri:
-    process.env.MONGO_URI ||
-    "mongodb+srv://pathanarfan8:99IyN7CAdeJQxmYC@cluster0.etaozwt.mongodb.net/jewelry",
+  mongoUri: process.env.MONGO_URI,
   clientUrl: process.env.CLIENT_URL || "http://localhost:3000",
   jwt: {
     accessSecret: process.env.JWT_ACCESS_SECRET || "change_this_access_secret",
